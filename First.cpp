3b97befa8acfb3dd77a9817f97c38da0e22105b0
Sean Silva <silvas@purdue.edu>
2013-07-04 21:45:55 -0700

    Part 2 of openglbook.com

--- First.cpp
+++ First.cpp
@@ -5,6 +5,30 @@
 #include <iostream>
 #include <sstream>
 
+const GLchar VertexShader[] = R"(
+#version 330
+layout(location = 0) in vec4 in_Position;
+layout(location = 1) in vec4 in_Color;
+out vec4 ex_Color;
+
+void main(void) {
+  gl_Position = in_Position;
+  ex_Color = in_Color;
+}
+
+)";
+
+const GLchar FragmentShader[] = R"(
+#version 330
+in vec4 ex_Color;
+out vec4 out_Color;
+
+void main(void) {
+  out_Color = ex_Color;
+}
+
+)";
+
 int CurrentWidth = 640;
 int CurrentHeight = 480;
 
@@ -19,6 +43,8 @@ void reshapeFunction(int Width, int Height) {
 void renderFunction() {
   ++FrameCount;
   glClear(GL_COLOR_BUFFER_BIT);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+
   glutSwapBuffers();
 }
 
@@ -48,7 +74,89 @@ void timerFunction(int Value) {
 
 void myErrorCallback(GLenum _source, GLenum _type, GLuint _id, GLenum _severity,
                      GLsizei _length, const char *_message, void *_userParam) {
+  // Protip:
+  // Put a breakpoint in this callback so that ARB_debug_output messages
+  // will trigger the breakpoint, and then look at the current stack trace.
+  //
+  // Source: <http://www.youtube.com/watch?v=btNVfUygvio>
   std::cerr << _message << "\n";
+}
+
+GLuint VertexShaderID;
+GLuint FragmentShaderID;
+GLuint ProgramID;
+void createShaders() {
+
+  VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+  const GLchar *VShaderSource = VertexShader;
+  glShaderSource(VertexShaderID, 1, &VShaderSource, nullptr);
+  glCompileShader(VertexShaderID);
+
+  FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+  const GLchar *FShaderSource = FragmentShader;
+  glShaderSource(FragmentShaderID, 1, &FShaderSource, nullptr);
+  glCompileShader(FragmentShaderID);
+
+  ProgramID = glCreateProgram();
+  glAttachShader(ProgramID, VertexShaderID);
+  glAttachShader(ProgramID, FragmentShaderID);
+  glLinkProgram(ProgramID);
+  glUseProgram(ProgramID);
+}
+
+void destroyShaders() {
+  glUseProgram(0);
+  glDetachShader(ProgramID, VertexShaderID);
+  glDetachShader(ProgramID, FragmentShaderID);
+  glDeleteShader(FragmentShaderID);
+  glDeleteShader(VertexShaderID);
+  glDeleteProgram(ProgramID);
+}
+
+GLuint VAOID;
+GLuint VBOID;
+GLuint ColorBufferID;
+void createVertexBufferObject() {
+  GLfloat Vertices[] = {
+    -0.8f, -0.8f, 0.0f, 1.0f,
+    0.0f, 0.8f, 0.0f, 1.0f,
+    0.8f, -0.8f, 0.0f, 1.0f
+  };
+  GLfloat Colors[] = {
+    1.0f, 0.0f, 0.0f, 1.0f,
+    0.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f, 1.0f
+  };
+
+  glGenVertexArrays(1, &VAOID);
+  glBindVertexArray(VAOID);
+
+  glGenBuffers(1, &VBOID);
+  glBindBuffer(GL_ARRAY_BUFFER, VBOID);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), &Vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+
+  glGenBuffers(1, &ColorBufferID);
+  glBindBuffer(GL_ARRAY_BUFFER, ColorBufferID);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), &Colors, GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(1);
+}
+
+void destroyVertexBufferObject() {
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &ColorBufferID);
+  glDeleteBuffers(1, &VBOID);
+  glBindVertexArray(0);
+  glDeleteVertexArrays(1, &VAOID);
+}
+
+void closeFunction() {
+  destroyShaders();
+  destroyVertexBufferObject();
 }
 
 int main(int argc, char **argv) {
@@ -74,7 +182,11 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  glClearColor(1.0, 0.0, 0.0, 0.0);
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  createShaders();
+  createVertexBufferObject();
+
+  glutCloseFunc(closeFunction);
   glutReshapeFunc(reshapeFunction);
   glutDisplayFunc(renderFunction);
   glutIdleFunc(idleFunction);
